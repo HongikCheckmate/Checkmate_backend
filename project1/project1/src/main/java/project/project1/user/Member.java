8b6477ec4ea760cd5f0e3f9908ed3d748ed14e1f
goddnsdlqhrh <goddnsdlqhrh@naver.com>
2025-04-02 18:50:04 +0900

    test.test

---
+++ project1/project1/src/main/java/project/project1/user/Member.java
@@ -0,0 +1,26 @@
+package project.project1.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone_number;
+}
