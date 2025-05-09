10c2f725a5e964722b287487c54b5574a8e6df50
goddnsdlqhrh <goddnsdlqhrh@naver.com>
2025-05-09 14:46:19 +0900

    목표설정페이지접근제한

--- project1/project1/src/main/java/project/project1/goal/GoalController.java
+++ project1/project1/src/main/java/project/project1/goal/GoalController.java
@@ -10,6 +10,7 @@ import project.project1.group.GroupService;
 import project.project1.user.CustomUserDetails;
 import project.project1.user.UserService;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,7 +31,19 @@ public class GoalController {
 
     // 목표설정 폼 HTML 화면 반환 (템플릿 엔진으로 렌더링)
     @GetMapping("/{groupId}/form")
-    public String showGoalForm(@PathVariable("groupId") Long groupId, Model model) {
+    public String showGoalForm(@PathVariable("groupId") Long groupId,
+                               @AuthenticationPrincipal CustomUserDetails userDetails,
+                               Model model) throws AccessDeniedException {
+
+        Long currentUserId = userDetails.getId();
+        Group group = groupService.findById(groupId);
+
+        boolean isMember = group.getMember().stream().anyMatch(member -> member.getId().equals(currentUserId));
+        boolean isManager = group.getManager().getId().equals(currentUserId);
+        if (!isMember || !isManager) {
+            throw new AccessDeniedException("접근 권한이 없습니다.");
+        }
+
         model.addAttribute("groupId", groupId);
         List<Group> groups = groupService.findAllGroups();
         model.addAttribute("certificationTypes", CertificationType.values());
