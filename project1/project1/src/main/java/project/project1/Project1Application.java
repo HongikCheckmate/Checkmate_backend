e91cd219a9d129e72a1256d41dc77284434361b2
KimHyeonUi <khy010802@gmail.com>
2025-10-03 16:26:57 +0900

    test: build & push 정상적인지 확인

--- project1/project1/src/main/java/project/project1/Project1Application.java
+++ project1/project1/src/main/java/project/project1/Project1Application.java
@@ -1,15 +1,19 @@
 package project.project1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
 public class Project1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
+
+		log.info("Test #1 for CI/CD!");
 	}
 
 }
