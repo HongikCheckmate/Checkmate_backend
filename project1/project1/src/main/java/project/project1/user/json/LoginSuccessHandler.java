c3e539227e6a742d774862d425b1cca7b58f6842
goddnsdlqhrh <goddnsdlqhrh@naver.com>
2025-10-25 16:30:43 +0900

    헤더에 accesstoken, refreshtoken 추가해서 전송

--- project1/project1/src/main/java/project/project1/user/json/LoginSuccessHandler.java
+++ project1/project1/src/main/java/project/project1/user/json/LoginSuccessHandler.java
@@ -13,10 +13,12 @@ import project.project1.user.SiteUser;
 import project.project1.user.UserRepository;
 import project.project1.user.jwt.JwtService;
 
+import java.io.IOException;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler  {
     private final JwtService jwtService;
     private final UserRepository userRepository;
 
@@ -25,7 +27,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) {
+                                        Authentication authentication)  throws IOException {
         String username = extractUsername(authentication); // 인증 정보에서 Username(id) 추출
         SiteUser siteuser = userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없음"));
@@ -33,13 +35,18 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         String accessToken = jwtService.createAccessToken(siteuser.getId(), username); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
         String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
 
-        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
-
         userRepository.findByUsername(username)
                 .ifPresent(user -> {
                     user.updateRefreshToken(refreshToken);
                     userRepository.saveAndFlush(user);
                 });
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK); // 200 OK 상태 코드
+
+        response.getWriter().write(
+                "{\"success\": true, \"accessToken\": \"" + accessToken + "\", \"refreshToken\": \"" + refreshToken + "\"}"
+        );
         log.info("로그인에 성공하였습니다. 아이디 : {}", username);
         log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
         log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
