9fdc2be708b36083f8140945385fdd9cd5c9976c
KimHyeonUi <khy010802@gmail.com>
2025-11-15 16:17:29 +0900

    fix: React Router 패턴 방식 수정

--- project1/project1/src/main/java/project/project1/ReactRouteController.java
+++ project1/project1/src/main/java/project/project1/ReactRouteController.java
@@ -1,8 +1,10 @@
-package project;
+package project.project1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class ReactRouteController {
 
